e14c9aa25124758784b40d1318bd445f2221929e
Alejandro Labra <alejandro.labra@gmail.com>
2012-06-14 13:42:35 -0400

    Change .gitignore, add filename and foldername for csharp. And add csharp example

---
+++ csharp/AcidTalks.UnitTest/AcidTalks.UnitTest.Test/CalculatorDivideFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AcidTalks.UnitTest.Test
+{
+    [TestFixture]
+    public class CalculatorDivideFixture
+    {
+        [Test]
+        public void divide_with_10_5_return_2()
+        {
+            //Arrangewith
+            var calc = new Calculator();
+
+            //Act
+            var result = calc.Divide(10, 5);
+
+            //Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [Test, ExpectedException(typeof(DivideByZeroException))]
+        public void divide_with_zero_then_return_throw_exception()
+        {
+            //Arrange
+            var calc = new Calculator();
+
+            //Act
+            calc.Divide(10, 0);
+        }
+    }
+}
---
+++ csharp/AcidTalks.UnitTest/AcidTalks.UnitTest/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace AcidTalks.UnitTest
+{
+    public class Calculator
+    {
+        public int Sum(int first, int second)
+        {
+            return first + second;
+        }
+
+        public int Subtract(int first, int second)
+        {
+            return first - second;
+        }
+
+        public int Divide(int first, int second)
+        {
+            return first/second;
+        }
+
+        public int Multiply(int first, int second)
+        {
+            return first * second;
+        }
+    }
+}
